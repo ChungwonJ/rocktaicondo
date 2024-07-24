2e1c99316f6a45fe77628b9e2832fcb98cb4a2ce
Chungwon <wjdcjddnjs54@naver.com>
2024-07-24 15:22:10 +0900

    ㅊ

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>락타이콘도 - 태국여행 및 콘도 예약 | 태국 부동산 및 예약 시스템</title>
-        <meta name="description" content="태국여행을 계획 중이라면, 방콕, 파타야 등 다양한 지역의 콘도를 쉽게 예약하세요. 락타이콘도는 최고의 태국 부동산 정보를 제공합니다." />
-        <meta name="keywords" content="태국여행, 태국 콘도 예약, 태국 부동산, 방콕 콘도 예약, 파타야 콘도 예약, 태국 여행 예약, 태국 부동산 시스템" />
+        <title>락타이콘도 - 태국여행 및 콘도 예약 | 락타이 콘도 - 태국 콘도 예약 시스템</title>
+        <meta name="description" content="태국여행을 계획 중이라면, 방콕, 파타야 등 다양한 지역의 콘도를 쉽게 예약하세요. 락타이콘도는 최고의 콘도 정보를 제공합니다." />
+        <meta name="keywords" content="태국여행, 태국 콘도 예약, 콘도, 방콕 콘도 예약, 파타야 콘도 예약, 태국 여행 예약, 콘도 시스템" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="태국여행 및 콘도 예약 - 락타이콘도 | 태국 부동산 및 예약 시스템" />
-        <meta property="og:description" content="태국여행을 계획 중이라면, 방콕, 파타야 등 다양한 지역의 콘도를 쉽게 예약하세요. 락타이콘도는 최고의 태국 부동산 정보를 제공합니다." />
+        <meta property="og:title" content="태국여행 및 콘도 예약 - 락타이콘도 | 태국 콘도 예약 시스템" />
+        <meta property="og:description" content="태국여행을 계획 중이라면, 방콕, 파타야 등 다양한 지역의 콘도를 쉽게 예약하세요. 락타이콘도는 최고의 콘도 정보를 제공합니다." />
         <meta property="og:image" content="/logo.jpg" />
         <meta property="og:url" content="https://rakthaicondo.vercel.app" />
         <meta name="naver-site-verification" content="a3c52e8fae8364875a0b84e0464ed52e3e2bef54" />
